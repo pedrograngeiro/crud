b554adf00265a2a97e0c6afe57005f7aae1b4f1d
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-10-18 11:02:18 -0300

    refazendo a tabela no read

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ const routes: Routes = [
   {
   path: "",
   component: HomeComponent
-  },{
+  },
+  {
     path: "products",
     component: ProductCrudComponent
   },
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule , LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -33,6 +33,11 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 
 
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -66,7 +71,10 @@ import { MatSortModule } from '@angular/material/sort';
     MatSortModule
 
   ],
-  providers: [],
+  providers: [{
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
