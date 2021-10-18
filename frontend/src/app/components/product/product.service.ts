21928140d15b9e88990d32d8bd7445e50fe272c6
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-10-18 15:20:17 -0300

    changes

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -29,4 +29,9 @@ export class ProductService {
     return this.http.get<Product[]>(this.baseUrl)
   }
 
+  readById(id: string): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`
+    return this.http.get<Product>(url)
+  }
+  
 }
