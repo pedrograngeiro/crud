b51c79a5ee04993f4408cce186994322b91ae310
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-10-20 10:31:48 -0300

    new

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
+import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-update',
@@ -6,11 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-update.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
+  product: Product;
 
-  constructor() { }
+  constructor(private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    });
   }
 
-  updateProduct(): void {}
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() =>
+      this.productService.showMessage('Produto atualizado com sucesso!'));
+    this.router.navigate(['/products']);
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+  }
 }
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -13,7 +13,7 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage(msg: string): void{
+  showMessage(msg: string): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: "right",
@@ -29,9 +29,18 @@ export class ProductService {
     return this.http.get<Product[]>(this.baseUrl)
   }
 
-  readById(id: string): Observable<Product> {
+  readById(id: number): Observable<Product> {
     const url = `${this.baseUrl}/${id}`
     return this.http.get<Product>(url)
   }
-  
+
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
+
+  delete(id: number): Observable<Product> {
+    const url = `$(this.baseUrl)/${id}`
+    return this.http.delete<Product>(url)
+  }
 }
